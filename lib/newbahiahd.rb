8bfae66af3e5485bc67457c8f096af503785aeae
Thiago Youssef <thiagodyoussef@gmail.com>
2022-02-17 10:20:16 -0300

    Create website integration

--- lib/newbahiahd.rb
+++ lib/newbahiahd.rb
@@ -17,7 +17,19 @@ module Newbahiahd
   end
   class F1SalesCustom::Email::Parser
     def parse
-      # ...
+      parsed_email = @email.body.colons_to_hash(/(Nome|Email|Telefone|Mensagem).*?:/, false)
+
+      {
+        source: {
+          name: F1SalesCustom::Email::Source.all[0][:name]
+        },
+        customer: {
+          name: parsed_email['nome'],
+          phone: parsed_email['telefone'],
+          email: parsed_email['email']
+        },
+        message: parsed_email['mensagem']
+      }
     end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,5 +3,35 @@ require 'byebug'
 
 RSpec.describe F1SalesCustom::Email::Parser do
   context 'when came from the website' do
+    let(:email) do
+      email = OpenStruct.new
+      email.to = [email: 'website@newbahiahd.f1sales.net']
+      email.subject = 'Proposta Website'
+      email.body = "De: Xandão Alencar <xandao.riro@gmail.com> <xandao.riro@gmail.com>\nAssunto: Proposta Website\n\nCorpo da mensagem:\nNome: Xandão Alencar\nEmail: xandao.riro@gmail.com\nTelefone: 71988288080\nMensagem: Mensagem HOME - Enviar Proposta"
+
+      email
+    end
+
+    let(:parsed_email) { described_class.new(email).parse }
+
+    it 'contains lead website a source name' do
+      expect(parsed_email[:source][:name]).to eq('Website')
+    end
+
+    it 'contains name' do
+      expect(parsed_email[:customer][:name]).to eq('Xandão Alencar')
+    end
+
+    it 'contains phone' do
+      expect(parsed_email[:customer][:phone]).to eq('71988288080')
+    end
+
+    it 'contains email' do
+      expect(parsed_email[:customer][:email]).to eq('xandao.riro@gmail.com')
+    end
+
+    it 'contains message' do
+      expect(parsed_email[:message]).to eq('Mensagem HOME - Enviar Proposta')
+    end
   end
 end
